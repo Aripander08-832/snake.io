d4f0ae92713164d58943e27aca044df081eb3c4f
Aripander08-832 <159452016+Aripander08-832@users.noreply.github.com>
2024-04-19 12:27:28 +0000

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -13,8 +13,8 @@ class Game{
 
 	init(){					
 		this.snakes[0] = new Snake(this.ctxSnake, "Bibhuti", 0);		
-		for(var i=0; i<25; i++) this.addSnake(ut.randomName(), 100);		
-		this.generateFoods(3000);			
+		for(var i=0; i<45; i++) this.addSnake(ut.randomName(), 100);		
+		this.generateFoods(7000);			
 	}
 
 	draw(){		
